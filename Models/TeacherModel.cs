e880d9db9ecde5c8fabb2af76aa7b641d5763c02
AbdolrezaKalantari <abdolrezakalantari2024@gmail.com>
2024-02-10 13:03:30 -0800

    Add Teacher Model

---
+++ Models/TeacherModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GroupProject.Models
+{
+    public class TeacherModel
+    {
+        public int teacherID { get; set; }
+        public string? teacherFistName { get; set; } = "Not Set";
+        public string? teacherLastName { get; set; } = "Not Set";
+        public string? teacherNationalCode { get; set; } = "Not Set";
+        public byte teacherTeachingArea { get; set; }
+    }
+}
