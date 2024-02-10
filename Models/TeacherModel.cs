41f41f7a8c0f7bc63f959f0a970130ab3bb6809a
Abdolreza Kalantari <abdolrezakalantari2024@gmail.com>
2024-02-10 14:14:02 -0800

    Edit Teacher Model

--- Models/TeacherModel.cs
+++ Models/TeacherModel.cs
@@ -3,9 +3,9 @@
     public class TeacherModel
     {
         public int teacherID { get; set; }
-        public string? teacherFistName { get; set; } = "Not Set";
-        public string? teacherLastName { get; set; } = "Not Set";
-        public string? teacherNationalCode { get; set; } = "Not Set";
+        public string teacherFistName { get; set; } = "Not Set";
+        public string teacherLastName { get; set; } = "Not Set";
+        public string teacherNationalCode { get; set; } = "Not Set";
         public byte teacherTeachingArea { get; set; }
     }
 }
