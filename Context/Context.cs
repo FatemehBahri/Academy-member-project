962dd214c0b6c5542c11b52137f67e39894bf3c4
AbdolrezaKalantari <abdolrezakalantari2024@gmail.com>
2024-02-10 12:50:15 -0800

    Upload New Project

---
+++ Context/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace GroupProject.Context
+{
+    public class Context : DbContext
+    {
+        #region DbSet
+
+
+        #endregion
+
+        readonly string _connectionString = "";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
