749afaa10a52900be76a6e253a60f4febb36497e
FatemehBahri <159642716+FatemehBahri@users.noreply.github.com>
2024-02-14 01:53:36 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IStudentRepository, Studentrepositories>();
 //builder.Services.AddDbContext<Context>(options =>
 //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
