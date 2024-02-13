185faffcdd7eb28b4d76c548cead03f2c96aae5b
Abdolreza Kalantari <abdolrezakalantari2024@gmail.com>
2024-02-13 08:58:38 -0800

    Update Teacher Model

--- Models/TeacherModel.cs
+++ Models/TeacherModel.cs
@@ -4,11 +4,14 @@ namespace GroupProject.Models
 {
     public class TeacherModel
     {
+        #region Teacher Parameter
         [Key]
         public int teacherID { get; set; }
         public string teacherFistName { get; set; } = "Not Set";
         public string teacherLastName { get; set; } = "Not Set";
         public string teacherNationalCode { get; set; } = "Not Set";
         public string teacherTeachingArea { get; set; }
+
+        #endregion
     }
 }
