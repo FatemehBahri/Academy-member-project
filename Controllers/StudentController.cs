aa74e97f01191fbd0178a1c58b6f57c6d827c75f
FatemehBahri <159642716+FatemehBahri@users.noreply.github.com>
2024-02-14 01:42:54 +0330

    Add files via upload
    update Student Controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,88 @@
+﻿using GroupProject.Models;
+using GroupProject.Repositories.Implimentations;
+using GroupProject.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+namespace GroupProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+        public StudentController()
+        {
+            _studentRepository = new Studentrepositories();
+        }
+
+        // GET: StudentController/Details
+        public ActionResult List()
+        {
+            List<StudentModel> studentModels = _studentRepository.GetAll();
+            return View(studentModels);
+        }
+
+        // GET: StudentController/Create
+       public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(StudentModel StudentModel)
+        {
+            bool result = await _studentRepository.Insert(StudentModel);
+            if (result)
+            {
+                return RedirectToAction("List", "Student");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            StudentModel StudentModel = await _studentRepository.Get(id);
+            return View(StudentModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(StudentModel StudentModel, StudentModel studentModel)
+        {
+            bool result = await _studentRepository.Update(StudentModel);
+            if (result)
+            {
+                return RedirectToAction("List", "Student");
+            }
+            else
+            {
+                StudentModel model = new StudentModel()
+                {
+                    StudentID = StudentModel.StudentID,
+                    studentFristname = StudentModel.StudentLastname,
+                    StudentLastname = StudentModel.StudentLastname,
+                    Age = StudentModel.Age,
+                    Email = StudentModel.Email,
+                    phonenumber = StudentModel.phonenumber
+
+                };
+
+                return View(studentModel);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            StudentModel StudentModel = await _studentRepository.Get(id);
+            return View(StudentModel);
+        }
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        public async Task<IActionResult>Delete(StudentModel StudentModel)
+        {
+            bool resalt=await _studentRepository.Delete(StudentModel.StudentID);
+            return RedirectToAction("List", "Student");
+        }
+    }
+}
