a235bf1ad899d7e3a456e94f5a9b2dfe0f90cfce
FatemehBahri <159642716+FatemehBahri@users.noreply.github.com>
2024-02-18 23:21:24 +0330

    Add files via upload
    update StudentModel

--- Models/StudentModel.cs
+++ Models/StudentModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace GroupProject.Models
 {
@@ -6,11 +7,19 @@ namespace GroupProject.Models
     {
         [Key]
         public int StudentID { get; set; }
+        [Display(Name = "Student Fristname")]
+        [Required(ErrorMessage ="Please Enter {0}")]
         public string studentFristname { get; set; }
+        [Display(Name = "Student Lastnamee")]
+        [Required(ErrorMessage = "Please Enter {0}")]
         public string StudentLastname { get; set; }
+        [Display(Name = "Age")]
+        [Required(ErrorMessage = "Please Enter {0}")]
         public int Age { get; set; }
-        public string phonenumber { get; set; }
-        public string Email { get; set; }
+        [DisplayFormat(DataFormatString = "{0:###-###-####}")]
+        public string? phonenumber { get; set; }
+        [EmailAddress(ErrorMessage = "The Email field is not a valid email address.")]
+        public string? Email { get; set; }
 
 
     }
